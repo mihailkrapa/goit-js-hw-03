a44e6fc1d43516c268e5793ddc763a76df694e99
mihailkrapa <138244914+mihailkrapa@users.noreply.github.com>
2024-02-19 20:29:21 +0100

    check HW

--- js/task-1.js
+++ js/task-1.js
@@ -6,7 +6,8 @@ function slugify(title) {
   return slug;
 }
 
-console.log(slugify("Arrays for begginers"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
